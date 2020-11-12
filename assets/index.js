729c1fec5f43e06b2c356d9ce6b5975f75cfa39d
Gabriel <rotgergabriel@gmail.com>
2020-11-12 18:52:00 -0300

    Calculadora finalizada

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,6 @@
 window.onload = function() { //Acciones tras cargar la página
     pantalla=document.getElementById("textoPantalla"); //elemento pantalla de salida
+    document.onkeydown = teclado; 
 }
 
 let x="0"; //guardar número en pantalla
@@ -53,3 +54,82 @@ function igualar() {
             xi=1; //se puede reiniciar la pantalla.
     }
 }
+
+function raizc() {
+    x=Math.sqrt(x);
+    pantalla.innerHTML=x;
+    op='no';
+    xi=1;
+}
+
+function porcent() {
+    x=x/100;
+    pantalla.innerHTML=x;
+    igualar();
+    xi=1;
+}
+
+function opuest() {
+    nx= Number(x);
+    nx= -nx
+    x=String(nx);
+    pantalla.innerHTML=x;
+}
+
+function inve() {
+    nx=Number(x);
+    nx=(1/nx);
+    x=String(nx);		 
+    pantalla.innerHTML=x;
+    xi=1; //reiniciar pantalla al pulsar otro número.
+}
+
+function retro(){ //Borrar sólo el último número escrito.
+    cifras=x.length; //hayar número de caracteres en pantalla
+    br=x.substr(cifras-1,cifras) //info del último caracter
+    x=x.substr(0,cifras-1) //quitar el ultimo caracter
+    if (x=="") {x="0";} //si ya no quedan caracteres, pondremos el 0
+    if (br==".") {coma=0;} //Si hemos quitado la coma, se permite escribirla de nuevo.
+    pantalla.innerHTML=x; //mostrar resultado en pantalla	 
+}
+
+function borradoParcial() {
+    pantalla.innerHTML=0; //Borrado de pantalla;
+    x=0; //Borrado indicador número pantalla.
+    coma=0; //reiniciamos también la coma					
+}
+
+function borradoTotal() {
+    pantalla.innerHTML=0; //poner pantalla a 0
+    x="0"; //reiniciar número en pantalla
+    coma=0; //reiniciar estado coma decimal 
+    ni=0 //indicador de número oculto a 0;
+    op="no" //borrar operación en curso.
+}
+
+function teclado (elEvento) { 
+        evento = elEvento || window.event;
+         k=evento.keyCode; //número de código de la tecla.
+         //teclas númericas del teclado alfamunérico
+        if (k>47 && k<58) { 
+            p=k-48; //buscar número a mostrar.
+            p=String(p) //convertir a cadena para poder añádir en pantalla.
+            numero(p); //enviar para mostrar en pantalla
+            }	
+         //Teclas del teclado númerico. Seguimos el mismo procedimiento que en el anterior.
+        if (k>95 && k<106) {
+            p=k-96;
+            p=String(p);
+            numero(p);
+            }
+         if (k==110 || k==190) {numero(".")} //teclas de coma decimal
+         if (k==106) {operar('*')} //tecla multiplicación
+         if (k==107) {operar('+')} //tecla suma
+         if (k==109) {operar('-')} //tecla resta
+         if (k==111) {operar('/')} //tecla división
+         if (k==32 || k==13) {igualar()} //Tecla igual: intro o barra espaciadora
+         if (k==46) {borradoTotal()} //Tecla borrado total: "supr"
+         if (k==8) {retro()} //Retroceso en escritura : tecla retroceso.
+         if (k==36) {borradoParcial()} //Tecla borrado parcial: tecla de inicio.
+}
+
